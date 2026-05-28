86b8fb670195893d8042d836b51e1197ca57da94
NAGOTHU VARSHITHA <2300032636@kluniversity.in>
2026-05-28 20:31:56 +0530

    Added alpha pyramid pattern

---
+++ patterns/alphaPyramid.py
@@ -0,0 +1,8 @@
+n=int(input("Enter the number of patterns:"))
+alph=65
+for i in range(0,n):
+    print(" "*(n-i),end="")
+    for j in range(0,i+1):
+        print(chr(alph),end="")
+        alph+=1
+print()
