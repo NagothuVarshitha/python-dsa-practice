dae9ad945bc3b6ab59eae0e051bfdcb0a5e72437
NagothuVarshitha <2300032636@kluniversity.in>
2026-06-03 11:48:33 +0530

    your message

---
+++ numsandMath/CountnumDivisibleby3or5(1 to N).py
@@ -0,0 +1,12 @@
+n=int(input("Enter the number: "))
+result=[]  #create an empty list and will store all answers.
+for i in range(1,n+1):
+    if i%3==0 and i%5==0:
+        result.append("FizzBuzz")
+    elif i%3==0:
+        result.append("Fizz")
+    elif i%5==0:
+        result.append("Buzz")
+    else:
+        result.append(str(i))
+print(result)
---
+++ numsandMath/largestNumLessthanNdivisibleby7.py
@@ -0,0 +1,5 @@
+N=int(input("Enter the number: "))
+K=int(input("Enter the number: "))
+rem=N%K
+ans=N-rem
+print(ans)
