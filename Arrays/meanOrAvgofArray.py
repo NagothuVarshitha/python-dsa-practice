9e8d78a1b259524d13b0db0dce91b8dca4b6b79c
NagothuVarshitha <2300032636@kluniversity.in>
2026-06-22 21:27:14 +0530

    Added program

--- Arrays/meanOrAvgofArray.py
+++ Arrays/meanOrAvgofArray.py
@@ -1,3 +1,11 @@
 arr=[10, 10, 20 , 5, 8, 7]
-ans=sum(arr)//len(arr)
-print(ans)
+total=0
+count=0
+for i in arr:
+    total+=i
+    count+=1
+print(total//count)
+
+
+#ans=sum(arr)//len(arr)
+#print(ans)
