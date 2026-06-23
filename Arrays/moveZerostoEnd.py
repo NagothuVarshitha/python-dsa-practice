16b1404b6c53b274fd6f10dce0950e3d975760fd
NagothuVarshitha <2300032636@kluniversity.in>
2026-06-23 12:21:58 +0530

    Added program

--- Arrays/moveZerostoEnd.py
+++ Arrays/moveZerostoEnd.py
@@ -1,9 +1,20 @@
-def movezerostoend(arr):
-    j=0
-    for i in range(len(arr)):
-        if arr[i]!=0:
-            arr[j],arr[i]=arr[i],arr[j]
-            j+=1
-    return arr
-arr=[1,0,3,0,12]
-print(movezerostoend(arr))
+#def movezerostoend(arr):  #Creates a function named movezerostoend that takes an array arr as input.
+    #j=0     #j keeps track of the position where the next non-zero element should be placed.
+   # for i in range(len(arr)):   #i goes from 0 to len(arr)-1.
+       # if arr[i]!=0:       #Check for non-zero: Only process elements that are not zero.
+           # arr[j],arr[i]=arr[i],arr[j]     #Swap:Move the non-zero element to position j.
+         #   j+=1        #Move j to the next position for future non-zero elements.
+    #return arr
+#arr=[1,0,3,0,12]
+#print(movezerostoend(arr))
+
+
+
+n=int(input())  #Single number
+arr=list(map(int,input().split())) #Array/List
+j=0
+for i in range(0,n):
+    if arr[i]!=0:
+        arr[j],arr[i]=arr[i],arr[j]
+        j+=1
+print(arr)
