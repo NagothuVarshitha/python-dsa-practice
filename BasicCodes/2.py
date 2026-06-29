fb09aa9d9ae8afecf718303dca420290cbe8456b
NagothuVarshitha <2300032636@kluniversity.in>
2026-06-29 18:54:32 +0530

    Add new code

---
+++ BasicCodes/2.py
@@ -0,0 +1,17 @@
+name=input("Enter your name: ")
+date=input("Enter your date: ")
+letter=f'''
+Dear {name},
+You are Selected!
+{date}
+'''
+print(letter)
+
+
+"""
+output:
+letter='''
+Dear <|name|>,
+You are Selected!
+<|date|>
+"""
