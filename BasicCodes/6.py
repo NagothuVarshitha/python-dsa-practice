2347c1f28a7e8435c298d9f38f5aaacda37c23b1
NagothuVarshitha <2300032636@kluniversity.in>
2026-07-08 11:54:37 +0530

    Add

---
+++ BasicCodes/6.py
@@ -0,0 +1,8 @@
+#Write a program to find the greatest of four numbers entered by the user
+n=list(map(int,input("Enter :").split()))
+maxi=n[0]       #n[0]:This works for positive, negative, and mixed numbers.
+for i in n:
+    if i>maxi:
+        maxi=i
+print("Maximum",maxi)
+
