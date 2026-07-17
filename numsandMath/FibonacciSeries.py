8a9771db5e14ba0e39f61c26e5b81b189ea59959
NagothuVarshitha <2300032636@kluniversity.in>
2026-07-17 12:07:30 +0530

    Add

--- numsandMath/FibonacciSeries.py
+++ numsandMath/FibonacciSeries.py
@@ -3,6 +3,6 @@ a=0     #current Fibonacci number-starts with 0 only
 b=1     #next Fibonacci number: 2nd term will be 1 only
 for i in range(0,n+1):
     print(a, end="")
-    c=a+b
+    c=a+b   #c will stores their sum temporarily
     a=b
     b=c
