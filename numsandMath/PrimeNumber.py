51bf2825bb803345d4c1ad1485eec16775da2e98
NagothuVarshitha <2300032636@kluniversity.in>
2026-07-17 12:06:58 +0530

    Add

--- numsandMath/PrimeNumber.py
+++ numsandMath/PrimeNumber.py
@@ -1,9 +1,9 @@
 n=int(input("Enter the number:"))
-count=0
-for i in range(1,n+1):
+count=0 #variable counts how many factors a number has.
+for i in range(1,n+1):  #loop checks every number from 1 to n.
     count=0
-    for j in range(1,i+1):
-        if i%j==0:
-            count+=1
-    if count==2:
+    for j in range(1,i+1):  #This loop finds all factors of i
+        if i%j==0:  #Checks whether j is a factor of i.
+            count=count+1    #Every time a factor is found, count increase
+    if count==2:    #A prime number has exactly 2 factors
         print(i,end=" ")
