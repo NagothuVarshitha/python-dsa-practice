dea37e5915c1e50f44ab1c5db497bcbe3385aeae
NagothuVarshitha <2300032636@kluniversity.in>
2026-07-25 11:51:52 +0530

    Add

--- Loops/countdigitsinanum.py
+++ Loops/countdigitsinanum.py
@@ -1,3 +1,9 @@
+#i/p:Enter a number: 12345
 n=int(input("Enter a number: "))
 count=0
-for i in range(1,n+1):
+"""while n > 0 → Continue until no digits are left.
+for i in range(1, n + 1) → Continue until i reaches n."""
+while n>0:
+    n=n//10
+    count+=1
+print(count)
