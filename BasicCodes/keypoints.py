4f68eb774b0a522274ce0fd4a22c43f9c9c2f007
NagothuVarshitha <2300032636@kluniversity.in>
2026-07-25 15:11:51 +0530

    Add

--- BasicCodes/keypoints.py
+++ BasicCodes/keypoints.py
@@ -33,7 +33,6 @@ for i in range(1, n + 1) → Continue until i reaches n.
 ✅reverse = reverse * 10 + digit
 •  Multiply by 10 → Creates an empty place on the right.
 •  Add the digit → Fills that empty place.
-
 ✅
 
 """
