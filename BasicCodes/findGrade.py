8382866f2b56229cac0f2c7f2b4711b733d846e3
NagothuVarshitha <2300032636@kluniversity.in>
2026-07-26 11:19:51 +0530

    Add

---
+++ BasicCodes/findGrade.py
@@ -0,0 +1,11 @@
+n=int(input("Enter a number: "))
+if n>=90:
+    print("A Grade")
+elif n>=80:
+    print("B Grade")
+elif n>=70:
+    print("C Grade")
+elif n>=60:
+    print("D Grade")
+else:
+    print("Fail")
