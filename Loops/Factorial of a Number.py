5f9dee7596d82db53307a29d200f942cb18a0671
NagothuVarshitha <2300032636@kluniversity.in>
2026-07-28 12:07:39 +0530

    Add

---
+++ Loops/Factorial of a Number.py
@@ -0,0 +1,13 @@
+"""n=int(input("Enter: "))
+fact=1
+for i in range(1,n+1):
+    fact=fact*i
+print(fact)"""
+
+n=int(input("Enter: "))
+fact=1
+i=1
+while i<=n:
+    fact=fact*i
+    i+=1
+print(fact)
--- Loops/countdigitsinanum.py
+++ Loops/countdigitsinanum.py
@@ -1,8 +1,8 @@
 #i/p:Enter a number: 12345
 n=int(input("Enter a number: "))
 count=0
-"""while n > 0 → Continue until no digits are left.
-for i in range(1, n + 1) → Continue until i reaches n."""
+"""while n > 0 → Continue until no digits are left-Use while when you don't know in advance how many times the loop will run.
+for i in range(1, n + 1) → Continue until i reaches n-Use for when you already know the number of repetitions."""
 while n>0:
     n=n//10
     count+=1
