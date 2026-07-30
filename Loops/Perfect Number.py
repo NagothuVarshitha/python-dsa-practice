e8c349274ceaba1df7b2e0fc465929bf40ddc6f5
NagothuVarshitha <2300032636@kluniversity.in>
2026-07-30 10:06:50 +0530

    Add

--- Loops/Perfect Number.py
+++ Loops/Perfect Number.py
@@ -4,12 +4,12 @@ factors of 6-1,2,3(We don't include the number itself)
 Now add them:1 + 2 + 3 = 6"""
 
 n=int(input("Enter: "))
-temp=n
-sum=0
-for i in range(1,n):
-    if n%i==0:
-        sum=sum+i
-if temp==sum:
+temp=n  # Store the original number
+sum=0   # Variable to store the sum of factors
+for i in range(1,n):    # Check every number from 1 to n-1
+    if n%i==0:  # If i divides n completely, then it is a factor
+        sum=sum+i   # Add the factor to the sum
+if temp==sum:   # Compare the sum of factors with the original number
     print("Perfect Number")
 else:
     print("Not Perfect Number")
