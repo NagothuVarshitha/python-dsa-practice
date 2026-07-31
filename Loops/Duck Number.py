a968f943336aa8b24fcc50e624c67701242c5288
NagothuVarshitha <2300032636@kluniversity.in>
2026-07-31 22:07:11 +0530

    Add

--- Loops/Duck Number.py
+++ Loops/Duck Number.py
@@ -3,10 +3,7 @@
 120 → Duck Number
 321 → Not Duck Number"""
 n=int(input("Enter a number: "))
-temp=n
-digit=str(n)
-while n>0:
-    if digit.endswith("0") and digit>=0:
+if "0" in n and not n.startswith("0"):
         print("Duck Number")
-    else:
-        print("Not Duck Number")
+else:
+    print("Not Duck Number")
