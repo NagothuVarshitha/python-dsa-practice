0d708c83e74fb33383f3b71867674d6652441f6b
NagothuVarshitha <2300032636@kluniversity.in>
2026-07-31 22:14:42 +0530

    Add

--- Loops/Peterson Number.py
+++ Loops/Peterson Number.py
@@ -6,12 +6,12 @@ Output: Peterson Number"""
 n=int(input("Enter a number: "))
 temp=n
 sum=0
-while n>0:
+while n>0:  # Process every digit
     rem=n%10
-    fact=1
-    for i in range(1,n+1):
+    fact=1    # Find factorial of current digit
+    for i in range(1,rem+1):   #for current digit--range(1, rem+1), Whole number?--range(1,n+1)
         fact=fact*i
-    sum=sum+fact
+    sum=sum+fact        # Add factorial
     n=n//10
 if temp==sum:
     print("Peterson Number")
