56f0b53fffe42ff7d4e8d22b12c43c2feef9c43c
NagothuVarshitha <2300032636@kluniversity.in>
2026-08-04 16:12:21 +0530

    Add

--- Loops/HCF.py
+++ Loops/HCF.py
@@ -1,6 +1,15 @@
-"""Input: 12 18
+"""HCF/GCD--The biggest number(factor) that divides both numbers completely.
+Input: 12 18
 Factors of 12: 1 2 3 4 6 12
 Factors of 18: 1 2 3 6 9 18
 Common factors: 1 2 3 6
 Highest Common Factor = 6"""
-n=int(input("Enter a number: "))
+#HCF can never be greater than the smaller n.o of i/p.
+
+n1,n2=map(int,input("Enter a number: ").split())
+hcf=1   #every +ve num have 1 as a common factor
+for i in range(1,min(n1,n2)+1):
+    if n1%i==0 and n2%i==0:       # If remainder is 0 for both numbers, then 'i' is a common factor.
+        hcf=i   #Stores the latest common factor(we don't write hcf += i becoz we don't want factors sum(1+2+3+6)❌ we want the last stored value will be the Highest Common Factor(6).
+print(hcf)
+
