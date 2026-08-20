f6cd9d5674d201c6a22fe9c7f2872461fb6ad8ac
NagothuVarshitha <2300032636@kluniversity.in>
2026-08-20 12:00:57 +0530

    Add Program

---
+++ patterns/Diamond Pattern.py
@@ -0,0 +1,28 @@
+"""
+   *
+  * * *
+ * * * * *
+* * * * * * *
+ * * * * *
+  * * *
+   *
+   Top: Spaces = n - i
+        Stars  = 2*i - 1
+   Bottom: Spaces = i
+           Stars  = 2*(n-i) - 1
+"""
+n = int(input("Enter: "))
+# ---------- Top half ----------
+for i in range(1, n + 1):   # Spaces decrease
+    for j in range(n - i):
+        print(" ", end=" ")
+    for k in range(2 * i - 1):  # Stars increase
+        print("*", end=" ")
+    print()
+# ---------- Bottom half ----------
+for i in range(1, n):   #the middle row is printed only once, so we use (1,n) for the bottom half.
+    for j in range(i):  # Spaces increase
+        print(" ", end=" ")
+    for k in range(2 * (n - i) - 1):    # Stars decrease
+        print("*", end=" ")
+    print()
--- patterns/Inverted Full Pyramid.py
+++ patterns/Inverted Full Pyramid.py
@@ -1,6 +1,15 @@
 """
-* * * * * * *
-  * * * * *
-    * * *
-      *
+* * * * * * *   Row 1 → 7
+  * * * * *     Row 2 → 5
+    * * *       Row 3 → 3
+      *         Row 4 → 1(n=4)
+Spaces = i - 1
+Stars  = 2*n - 2*i + 1
 """
+n=int(input("Enter: "))
+for i in range(1,n+1):
+    for j in range(i-1):
+        print(" ",end=" ")
+    for k in range(2*n-2*i+1):
+        print("* ",end=" ")
+    print()
